f5c7abd867b21f6c75442fbf61fdbe4744867e8f
Mason Britsch <mason.britsch@gmail.com>
2022-05-02 13:05:30 -0700

    commit

---
+++ PYTHON/fundamentals/fundamentals/functions_basics_II/func2.py
@@ -0,0 +1,44 @@
+#countdown
+# arr = []
+# for i in range(5,0,-1):
+#     arr.append(i)
+# print(arr)
+
+#Print & Return
+# arr=[1,2]
+# def printNreturn(x,y):
+#     print(x)
+#     return(y)
+# printing=printNreturn(arr[0],arr[1])
+# print(printing)
+
+#First Plus Length
+# def fpl(a,b):
+#     sum=a + b
+#     print(sum)
+# arr=[5,4,3,2,1]
+# fpl(arr[0], len(arr))
+
+#Values Greater than Second
+# def values_greater_than_second(val):
+#     val2 = []
+#     if len(val) <= 2:
+#         return False
+#     for i in range(0,len(val)):
+#         if i > val[1]:
+#             val2.append(i)
+#     print(len(val2))
+#     return val2
+# tempval = [5,2,3,2,1,4]
+# final = values_greater_than_second(tempval)
+# print(final)
+
+#This Length, That Value
+# def length_and_value(a,b):
+#     val = []
+#     for i in range(0,b+1):
+#         if len(val) < b:
+#             val.append(a)
+#     print(val)
+# length_and_value(5,7)
+
