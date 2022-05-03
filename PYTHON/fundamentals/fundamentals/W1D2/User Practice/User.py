aa362c28b3d75467c4410a80c38f8ee4f3bbeac6
Mason Britsch <mason.britsch@gmail.com>
2022-05-02 21:11:45 -0700

    comit

---
+++ PYTHON/fundamentals/fundamentals/W1D2/User Practice/User.py
@@ -0,0 +1,19 @@
+class User:
+    
+    bank_name = "Dojo Bank"
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.account_balance = 10
+
+    def make_withdrawl(self, amount):
+        self.account_balance -= amount
+        return self
+    
+    def display_user_balance(self):
+        print("User:", self.name,", Balance:", self.account_balance)
+
+
+Guido = User("Guido", "Guido@python.com")
+
+Guido.display_user_balance()
