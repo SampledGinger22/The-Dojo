8d170ceed46e4bcd5b461e3ff9caa68ec8b564a7
Mason Britsch <mason.britsch@gmail.com>
2022-05-04 19:23:36 -0700

    commit

--- PYTHON/flask/fundamentals/Playground/server.py
+++ PYTHON/flask/fundamentals/Playground/server.py
@@ -1,13 +1,18 @@
 from flask import Flask, render_template  # added render_template!
 app = Flask(__name__)                     
     
-@app.route('/')                           
+@app.route("/play/<int:number>/<color>")
+def play(number, color):
+    return render_template("boxes.html", times = number, color = color)
+
+@app.route('/hello/testme')                           
 def index():
     return render_template("index.html", phrase="hello", times=5)  
 
 @app.route("/hello/<string:banana>/<int:num>")
 def hello(banana,num):
     return render_template("hello.html",banana=banana,num=num)
+
 if __name__=="__main__":
     app.run(debug=True)                   
 
