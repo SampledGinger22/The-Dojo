dd5d3c13ee07fbbdecd1825f6be311ef7a54c95f
Mason Britsch <mason.britsch@gmail.com>
2022-05-06 15:23:10 -0700

    commit

--- PYTHON/flask/fundamentals/DojoSurvey/server.py
+++ PYTHON/flask/fundamentals/DojoSurvey/server.py
@@ -1,10 +1,24 @@
-from flask import Flask, render_template, redirect, session
+from unicodedata import name
+from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
 app.secret_key = "hellothere"
 
 @app.route('/')
-def display():
+def displayform():
     return render_template("index.html")
+
+@app.route('/process', methods=["POST"])
+def process():
+    session["name"] = "full_name"
+    session["location"] = "fav_location"
+    session["language"] = "fav_language"
+    session["comment"] = "comments"
+    print(request.form["full_name"])
+    return redirect("/")
+
+@app.route("/results")
+def results():
+    return render_template("return.html")
 
 if __name__=="__main__":
     app.run(debug=True)
