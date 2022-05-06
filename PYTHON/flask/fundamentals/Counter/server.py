f98b3f531cc0d2fae568415c62866c4fab2a1bbd
Mason Britsch <mason.britsch@gmail.com>
2022-05-05 21:31:02 -0700

    commit

--- PYTHON/flask/fundamentals/Counter/server.py
+++ PYTHON/flask/fundamentals/Counter/server.py
@@ -1,9 +1,23 @@
-from flask import Flask, render_template, request, redirect, session
-app = Flask(__name__)  
+from flask import Flask, render_template, redirect, session
+app = Flask(__name__)
+app.secret_key = "hellothere"
 
 @app.route('/')
-def index():
+def refreshcounter():
+    if "visits" in session:
+        session["visits"] += 1
+    else: 
+        session["visits"] = 1
     return render_template("index.html")
 
-if __name__=="__main__":   
+@app.route("/count_up")
+def count():
+    return redirect("/")
+
+@app.route("/destroy_session")
+def clear():
+    session.pop("visits")
+    return redirect("/")
+
+if __name__=="__main__":
     app.run(debug=True)
