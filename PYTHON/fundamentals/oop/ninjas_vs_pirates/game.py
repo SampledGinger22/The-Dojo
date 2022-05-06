9f37e51ea4730ff0f37fd169b9fbf6c9f08b74f4
Mason Britsch <mason.britsch@gmail.com>
2022-05-06 16:21:01 -0700

    commit

--- PYTHON/fundamentals/oop/ninjas_vs_pirates/game.py
+++ PYTHON/fundamentals/oop/ninjas_vs_pirates/game.py
@@ -1,12 +1,9 @@
-from random import random
+from random import randint
 from classes.ninja import Ninja
 from classes.pirate import Pirate
-from classes.dice import Dice
 
 michelangelo = Ninja("Michelanglo")
 jack_sparrow = Pirate("Jack Sparrow")
-D20=Dice(20)
-D6=Dice(6)
 
 
 # michelangelo.attack(jack_sparrow)
@@ -21,36 +18,21 @@ D6=Dice(6)
 
 class Game:
 
+    def __init__(self, ninja, pirate):
+        self.pirate = pirate
+        self.ninja = ninja
 
+    def startingroll(self, pirate_player, ninja_player):
+        print(pirate_player, ":", self.D_roll(1,20))
+        print(ninja_player,":", self.D_roll(1,20))
 
-    def __init__(self, ninjaplayer = michelangelo, pirateplayer = jack_sparrow):
-        self.ninja_player = ninjaplayer
-        self.pirate_player = pirateplayer
-        self.dice20 = D20
+    def D_roll(lowval,highval):
+        return(randint(lowval,highval))
 
-    def startgame(self):
-        if self.initiative_roll == "True" or "true":
-            print("Rolling for who starts")
-            return self.startingroll
-        # else: 
-        #     exit
-
-    def startingroll(self):
-        print(self.pirate_player, ":", self.D20_roll)
-        print(self.ninja_player,":", self.D20_roll)
-
-    def D20_roll(lowval,highval):
-        roll = random(lowval, highval)
-        print(roll)
-
-    def D6_roll():
-        roll = random(D6.lowcount, D6.highcount)
-        print(roll)
-
-    initiative_roll = input("Start Scenario? True or False: ")
-    print("Start Game is: " + initiative_roll)
-    if initiative_roll == "True" or "true":
-        print("Rolling for who starts!! ") and D20_roll(1,20)
-        
+initiative_roll = input("Start Scenario? True or False: ")
+if initiative_roll == "True" or "true":
+    print("Rolling for who starts!! ")
+Game.startingroll(Game, michelangelo.name, jack_sparrow.name)
+    
 
 
