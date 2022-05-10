8d502fa8acece69549be48c45901a5efdecf5949
Mason Britsch <mason.britsch@gmail.com>
2022-05-10 15:45:58 -0700

    commit

---
+++ PYTHON/flask_mysql/db_connection/Users CR/server.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, redirect
+from user import User
+app = Flask(__name__)
+
+@app.route('/')
+def form():
+    return render_template(form.html)
+
+@app.route('/create_user', methods=["POST"])
+def create_user():
+    data = {
+        "fname": request.form["fname"],
+        "lname" : request.form["lname"],
+        "email" : request.form["email"]
+    }
+    User.save(data)
+    return redirect('/tableresults')
+
+@app.route('/tableresults')
+def index():
+    users = User.get_all()
+    print(users)
+    return render_template("record.html", all_users = users)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
