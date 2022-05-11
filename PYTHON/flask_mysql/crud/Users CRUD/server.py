60501e42b2a19c230c1510e15dd1888672845f33
Mason Britsch <mason.britsch@gmail.com>
2022-05-10 20:42:53 -0700

    commit

--- PYTHON/flask_mysql/crud/Users CRUD/server.py
+++ PYTHON/flask_mysql/crud/Users CRUD/server.py
@@ -4,9 +4,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def form():
+    return redirect ("/users/new")
+
+@app.route('/users/new')
+def form():
     return render_template("form.html")
 
-@app.route('/create_user', methods=["POST"])
+@app.route('/userspost', methods=["POST"])
 def create_user():
     data = {
         "fname": request.form["fname"],
@@ -14,13 +18,35 @@ def create_user():
         "email" : request.form["email"]
     }
     User.save(data)
-    return redirect('/tableresults')
+    return redirect('/users')
 
-@app.route('/tableresults')
+@app.route('/users')
 def index():
     users = User.get_all()
     print(users)
     return render_template("record.html", all_users = users)
+
+@app.route('/users/<int:id>/show')
+def show(id):
+    data = {
+        'id' : id
+            }
+    return render_template('show_user.html', user = User.get_one(data))
+
+@app.route('/users/<int:id>/edit')
+def edit(id):
+    data = {
+        'id' : id
+            }
+    return render_template('edit_user.html', user = User.get_one(data))
+
+@app.route('/users/<int:id>/destroy')
+def delete(id):
+    data = {
+        'id' : id
+            }
+    User.delete(data)
+    return redirect('/users')
 
 if __name__ == "__main__":
     app.run(debug=True)
