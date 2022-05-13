298581b59bdf3d46d32100dbda7b100930cdf53f
Mason Britsch <mason.britsch@gmail.com>
2022-05-13 07:37:42 -0700

    commit

---
+++ PYTHON/flask_mysql/validation/Login and Registration/flask_app/controllers/users.py
@@ -0,0 +1,62 @@
+from flask import render_template, redirect, request, session, flash
+from flask_app import app
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
+from flask_app.models.user import User
+
+@app.route('/')
+def landing():
+    return redirect('/reg_login')
+
+@app.route('/reg_login')
+def reg_login():
+    return render_template('login.html')
+
+@app.route('/register', methods=['POST'])
+def register():
+
+    if not User.validate_user_info(request.form):
+        return redirect('/reg_login')
+
+    pw_hash = bcrypt.generate_password_hash(request.form['password'])
+    print(pw_hash)
+
+    data = {
+        'first_name' : request.form['first_name'],
+        'last_name': request.form['last_name'],
+        'email': request.form['email'],
+        'password': pw_hash
+    }
+    user_id = User.save(data)
+    session['user_id'] = user_id
+    return redirect('/reg_login')
+
+
+# LOOK FOR SHIT HERE
+@app.route('/login', methods=['POST'])
+def login():
+    data = {
+        'email':request.form['email']
+    }
+    user_in_db = User.get_by_email(data)
+    if not user_in_db:
+        flash('Incorrect email or password')
+        return redirect('/reg_login')
+    if not bcrypt.check_password_hash(user_in_db.password, request.form['password']):
+        flash('Incorrect email or password')
+        return redirect('/reg_login')
+    session['user_id'] = user_in_db.id
+    return redirect('/success')
+
+@app.route('/success')
+def success():
+    data = {
+        'id': session['user_id']
+    }
+    user = User.get_one(data)
+    return render_template('success.html', user = user)
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/reg_login')
---
+++ PYTHON/flask_mysql/validation/Login and Registration/flask_app/models/user.py
@@ -0,0 +1,59 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask import flash
+import re
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]+$') 
+
+class User:
+    def __init__(self, data):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.password = data['password']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+    @staticmethod
+    def validate_user_info(user):
+        is_valid = True
+        if len(user['first_name']) < 2:
+            flash("First name must be at least 2 characters.")
+            is_valid = False
+        if len(user['last_name']) < 2:
+            flash("Last name must be at least 2 characters.")
+            is_valid = False
+        if not (user['first_name']).isalpha():
+            flash('First name must only contain letters!')
+            is_valid = False
+        if not (user['last_name']).isalpha():
+            flash('Last name must only contain letters!')
+            is_valid = False
+        if not EMAIL_REGEX.match(user['email']):
+            flash('Invalid email address')
+            is_valid = False
+        if not (user['conf_password'] == user['password']):
+            flash('Passwords do not match!')
+            is_valid = False
+        if len(user['password']) < 8:
+            flash("Password must be at least 8 characters.")
+            is_valid = False
+        return is_valid
+
+    @classmethod
+    def save(cls, data):
+        query = "INSERT INTO users ( first_name , last_name , email , password , created_at, updated_at ) VALUES ( %(first_name)s , %(last_name)s , %(email)s , %(password)s , NOW() , NOW() );"
+        return connectToMySQL('users_login').query_db( query, data )
+
+    @classmethod
+    def get_by_email(cls, data):
+        query = "SELECT * FROM users WHERE user.email = %(email)s;"
+        result = connectToMySQL('users_login').query_db( query , data )
+        if len(result) < 1:
+            return False
+        return cls(result[0])
+
+    @classmethod
+    def get_one(cls, data):
+        query = "SELECT * FROM users WHERE id = %(id)s;"
+        result = connectToMySQL('users_login').query_db(query, data)
+        return cls(result[0])
