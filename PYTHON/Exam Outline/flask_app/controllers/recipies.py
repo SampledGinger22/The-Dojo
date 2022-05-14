305f1f863ab44823155b73d6170a1b11d3a18327
Mason Britsch <mason.britsch@gmail.com>
2022-05-14 16:40:35 -0700

    commit

--- PYTHON/Exam Outline/flask_app/controllers/recipies.py
+++ PYTHON/Exam Outline/flask_app/controllers/recipies.py
@@ -1,5 +1,5 @@
 from flask_app import app
-from flask import render_template,redirect,request
+from flask import render_template,redirect,request,session
 from flask_app.models.user import User
 from flask_app.models.recipe import Recipe
 
@@ -10,10 +10,20 @@ def new_recipe():
 @app.route('/saverecipe', methods=["POST"])
 def save_recipe():
     data = {
+        'user_id': session['user_id']
         **request.form
     }
     Recipe.save_recipe(data)
     return redirect('/user_dash')
+
+@app.route('/viewrecipe/<int:id>')
+def view_recipe(id):
+    data = {
+        'id':id
+    }
+    recipe = Recipe.get_recipe(data)
+    print(recipe)
+    return render_template('view_recipe.html', recipes = recipe)
 
 @app.route('/recipe/<int:id>/edit')
 def edit(id):
