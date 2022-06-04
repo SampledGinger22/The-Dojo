2abda0687d08a954356f8e36e99c5e3b29bc9e52
Mason Britsch <mason.britsch@gmail.com>
2022-06-03 18:37:29 -0700

    commit

--- C#/Fundamentals/algosamples/Program.cs
+++ C#/Fundamentals/algosamples/Program.cs
@@ -1,31 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-// for(int i = 1; i <= 255; i++) {
-//     Console.WriteLine(i);
-// }
+for(int i = 1; i <= 255; i++) {
+    Console.WriteLine(i);
+}
 
-// for(int i = 1; i <= 100; i++) {
-//     if(i%3==0 && i%5==0){
-//     }
-//     else if(i%3==0){
-//         Console.WriteLine(i);
-//     }
-//     else if(i%5==0){
-//         Console.WriteLine(i);
-//     }
-// }
+for(int i = 1; i <= 100; i++) {
+    if(i%3==0 && i%5==0){
+    }
+    else if(i%3==0){
+        Console.WriteLine(i);
+    }
+    else if(i%5==0){
+        Console.WriteLine(i);
+    }
+}
 
 
-// for(int i = 1; i <= 255; i++) {
-//     if(i%3==0 && i%5==0){
-//         Console.WriteLine("FizzBuzz");
-//     }
-//     else if(i%3==0){
-//         Console.WriteLine("Fizz");
-//     }
-//     else if(i%5==0){
-//         Console.WriteLine("Buzz");
-//     }
-//     else Console.WriteLine(i);
-// }
+for(int i = 1; i <= 255; i++) {
+    if(i%3==0 && i%5==0){
+        Console.WriteLine("FizzBuzz");
+    }
+    else if(i%3==0){
+        Console.WriteLine("Fizz");
+    }
+    else if(i%5==0){
+        Console.WriteLine("Buzz");
+    }
+    else Console.WriteLine(i);
+}
 
