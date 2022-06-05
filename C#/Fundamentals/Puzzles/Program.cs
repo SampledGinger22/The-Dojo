bd8d3d583a3f51ab6bc1c57f105c187de7c1ff22
Mason Britsch <mason.britsch@gmail.com>
2022-06-04 22:14:10 -0700

    commit

---
+++ C#/Fundamentals/Puzzles/Program.cs
@@ -0,0 +1,62 @@
+﻿// static void RandomArray(){
+//     int[] arr = new int[10];
+//     Random Rand = new Random();
+//     for(int i = 0; i < 10; i++){
+//         arr[i] = Rand.Next(5,26);
+//     }
+//     int MaxVal = arr[0];
+//     int MinVal = arr[0];
+//     int Sum = arr[0];
+//     for(int j = 1; j < arr.Length-1; j++){
+//         if(arr[j] > MaxVal){
+//             MaxVal = arr[j];
+//         }
+//         if(arr[j] < MinVal){
+//             MinVal = arr[j];
+//         }
+//         Sum += arr[j];
+//     }
+//     Console.WriteLine(MaxVal.ToString());
+//     Console.WriteLine(MinVal.ToString());
+//     Console.WriteLine(Sum.ToString());
+// }
+// RandomArray();
+
+// static void TossCoin(int num){
+//     int FlipCount = num;
+//     int i = 0;
+//     while(i < FlipCount){
+//         i++;
+//         Console.WriteLine("Tossing a Coin!");
+//         Random rand = new Random();
+//         int flip = rand.Next(0, 2);
+//         if(flip == 1){
+//             Console.WriteLine("Heads");
+//         }
+//         if(flip == 0){
+//             Console.WriteLine("Tails");
+//         }
+//     }
+// }
+// TossCoin(5);
+
+static void Names(){
+    List<string> allnames = new List<string>();
+    allnames.Add("Tiffany");
+    allnames.Add("Charlie");
+    allnames.Add("Todd");
+    allnames.Add("Geneva");
+    allnames.Add("Sydney");
+
+    List<string> namelength = new List<string>();
+    for(int i = 0; i < allnames.Count(); i++){
+        string tempname = allnames[i];
+        if(tempname.Length > 4){
+            namelength.Add(tempname);
+        }
+    }
+    for(int i = 0; i < namelength.Count(); i++){
+        Console.WriteLine(namelength[i]);
+    }
+}
+Names();
