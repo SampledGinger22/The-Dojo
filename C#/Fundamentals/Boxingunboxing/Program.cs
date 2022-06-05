c92dcb1ab4acf1526c0582bbb33633279133850d
Mason Britsch <mason.britsch@gmail.com>
2022-06-04 17:57:55 -0700

    commit

---
+++ C#/Fundamentals/Boxingunboxing/Program.cs
@@ -0,0 +1,22 @@
+﻿List<object> templist = new List<object>();
+
+templist.Add(7);
+templist.Add(28);
+templist.Add(-1);
+templist.Add(true);
+templist.Add("Chair"); 
+
+int sum = 0;
+foreach(object val in templist) {
+    if(val is int) {
+        Console.WriteLine(val);
+        sum += (int)val;
+    }
+    else if(val is bool) {
+        Console.WriteLine(val);
+    }
+    else if(val is string){
+        Console.WriteLine(val);
+    }
+}
+Console.WriteLine(sum);
