d12b77d6604846faac3d0bad1ecfc6b2699e62b9
Mason Britsch <mason.britsch@gmail.com>
2022-06-09 12:09:45 -0700

    Commit

--- C#/ASP.NET/ASP MVC II/Dojo Survey with Validation/Models/ninjasMDL.cs
+++ C#/ASP.NET/ASP MVC II/Dojo Survey with Validation/Models/ninjasMDL.cs
@@ -1,10 +1,15 @@
 #pragma warning disable CS8618
+using System.ComponentModel.DataAnnotations;
 
 namespace Dojo_Survey_with_Validation.Models {
     public class Ninja {
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Location { get; set; }
+        [Required]
         public string Language { get; set; }
+        [Required]
         public string Comment { get; set; }
     }
 }
