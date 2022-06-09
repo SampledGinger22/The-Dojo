6049393cf43c799eaa4a881653f51782fcb3f8aa
Mason Britsch <mason.britsch@gmail.com>
2022-06-09 12:37:58 -0700

    commit

--- C#/ASP.NET/ASP MVC II/Dojo Survey with Validation/Models/ninjasMDL.cs
+++ C#/ASP.NET/ASP MVC II/Dojo Survey with Validation/Models/ninjasMDL.cs
@@ -6,11 +6,14 @@ namespace Dojo_Survey_with_Validation.Models {
         [Required]
         [MinLength(2, ErrorMessage = "Name must be at least two characters long")]
         public string Name { get; set; }
+
         [Required]
         public string Location { get; set; }
+
         [Required]
         public string Language { get; set; }
+
         [MinLength(20, ErrorMessage = "Must contain at least 20 characters")]
-        public string Comment { get; set; } = null!;
+        public string? Comment { get; set; }
     }
 }
