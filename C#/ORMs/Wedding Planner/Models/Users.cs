7c09137f9486ff4e2a436df54f58292e4b8a1133
Mason Britsch <mason.britsch@gmail.com>
2022-06-16 13:47:38 -0700

    commit

---
+++ C#/ORMs/Wedding Planner/Models/Users.cs
@@ -0,0 +1,29 @@
+#pragma warning disable CS8618
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Wedding_Planner.Models;
+
+public class User 
+{
+    [Key]
+    public int id { get;set; }
+    [Required]
+    [MinLength(2, ErrorMessage = "First name must be at least two characters in length")]
+    public string FirstName { get;set; }
+    [Required]
+    [MinLength(2, ErrorMessage = "Last name must be at least two characters in length")]
+    public string LastName { get;set; }
+
+    [Required]
+    [EmailAddress]
+    public string Email { get;set; }
+    [Required]
+    [MinLength(8, ErrorMessage = "Password must be at least 8 characters in length.")]
+    public string Password { get;set; }
+
+    [NotMapped]
+    [Compare("Password")]
+    [DataType(DataType.Password)]
+    public string Confirm { get;set; }
+}
