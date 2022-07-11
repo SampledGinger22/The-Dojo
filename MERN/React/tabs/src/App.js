70f3c274b53611124d46956efe4ecbc291316ffc
Mason Britsch <mason.britsch@gmail.com>
2022-07-11 14:53:05 -0700

    commit

---
+++ MERN/React/tabs/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App(arr = []) {
+
+  const ArrHandler = (e, array) => {
+
+  }
+
+  return (
+    <div className="App">
+      <div className='flexrow'>
+      { arr.map( (string, i) => 
+        <
+      ) }
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
